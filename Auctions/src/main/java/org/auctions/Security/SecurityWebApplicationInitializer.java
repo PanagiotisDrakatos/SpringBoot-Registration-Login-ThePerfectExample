6dcffeefc36ac2deb6308a620227fd08c6bb411f
panagiotis <panagiotisdrakatos@gmail.com>
2017-09-05 00:16:52 +0300

    Validate Project

--- Auctions/src/main/java/org/auctions/Security/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package org.auctions.Security;
-
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
--- Auctions/src/main/java/org/auctions/Service/UserService.java
+++
@@ -1,9 +0,0 @@
-package org.auctions.Service;
-
-
-import org.auctions.Model.User;
-
-public interface UserService {
-    public User findUserByEmail(String email);
-    public void createUserAccount(User user);
-}
